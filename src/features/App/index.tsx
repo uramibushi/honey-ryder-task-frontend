e8cdc309632a89d713dffc408962b88d99ce7d4f
akagi kenji <akagi-kenji@taikisystem.jp>
2022-05-09 22:48:27 +0900

    エントリーポイント修正

--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import { PrimaryButton } from 'components/ui-elements/Button/';
-import { InputField } from 'components/ui-elements/InputField';
-import Stack from '@mui/material/Stack';
-
-const handleClick : React.MouseEventHandler<HTMLButtonElement> = () => {
-};
+import CreateTaskForm from 'features/Tasks/components/CreateTaskForm';
 
 const App = () => (
   <div>
-    <h1>Honey Ryder Task</h1>
-    <div>
-      <Stack spacing={2}>
-        <InputField label="input1" />
-        <InputField label="input2" />
-        <InputField label="input3" />
-        <PrimaryButton onClick={handleClick}>保存</PrimaryButton>
-      </Stack>
-    </div>
+    <CreateTaskForm />
   </div>
 );
 
