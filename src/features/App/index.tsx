e1c2c08710e13efc4207baf9a4e70c552ca24516
akagi kenji <akagi-kenji@taikisystem.jp>
2022-05-22 22:48:52 +0900

    ダークモード設定

--- src/features/App/index.tsx
+++ src/features/App/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { CreateTaskForm } from 'features/Tasks/components/CreateTaskForm';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { colors, CssBaseline } from '@mui/material';
+
+const darkTheme = createTheme({
+  palette: {
+    primary: {
+      main: colors.yellow[600],
+    },
+    mode: 'dark',
+  },
+});
 
 const App = () => (
-  <div>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <CreateTaskForm />
-  </div>
+  </ThemeProvider>
 );
 
 export default App;
