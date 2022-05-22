fce996e581beb6518f0a4fd75869b80701fc7b45
akagi kenji <akagi-kenji@taikisystem.jp>
2022-05-22 19:07:13 +0900

    タスク新規登録をカスタムフックを使用する方式に変更

--- src/features/Tasks/components/CreateTaskForm.tsx
+++ src/features/Tasks/components/CreateTaskForm.tsx
@@ -1,38 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { PrimaryButton } from 'components/ui-elements/Button';
-import { TextField, TextArea } from 'components/ui-elements/TextField';
+import { DateField, TextArea, TextField } from 'components/ui-elements/TextField';
 import Stack from '@mui/material/Stack';
-import { apiClient } from 'shared/web/apiClient';
+import { useCreateNewTask } from 'features/Tasks/hooks';
 
 export const CreateTaskForm = () => {
-  type TaskType = {
-    title: string,
-    detail: string | undefined,
-    dueDate: string | undefined
-  }
-
-  const [task, setTask] = useState<TaskType>({
-    title: '',
-    detail: undefined,
-    dueDate: undefined,
-  });
-
-  const createNewTask : React.MouseEventHandler<HTMLButtonElement> = () => {
-    apiClient.post('Tasks/CreateNewTask', task);
-  };
-
+  const { task, setTask, createNewTask } = useCreateNewTask();
   return (
     <div>
       <h1>Honey Ryder Task</h1>
       <div>
         <Stack spacing={2}>
           <TextField label="タスクタイトル" value={task.title} onChange={(e) => setTask({ ...task, title: e.target.value })} />
-          <TextArea
-            label="タスク詳細"
-            value={task.detail}
-            onChange={(e) => setTask({ ...task, detail: e.target.value })}
-          />
-          <TextField label="タスク期限" value={task.dueDate} onChange={(e) => setTask({ ...task, dueDate: e.target.value })} />
+          <TextArea label="タスク詳細" value={task.detail} onChange={(e) => setTask({ ...task, detail: e.target.value })} />
+          <DateField label="タスク期限" value={task.dueDate} onChange={(e) => setTask({ ...task, dueDate: e.target.value })} />
           <PrimaryButton onClick={createNewTask}>保存</PrimaryButton>
         </Stack>
       </div>
